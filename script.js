e8e7b1ce8a7e488f46df161e1dc28be16b44867e
Cecelia Braswell <ceceliabraswell@yahoo.com>
2023-09-10 22:30:43 -0700

    sixth commit

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const profileMenu = document.getElementById("profileMenu");
+const sidebarActivity = document.getElementById("sidebarActivity");
+const moreLink = document.getElementById("showMoreLink");
 
-let toggleMenu = () => {
+function toggleMenu() {
     profileMenu.classList.toggle("open-menu");
 }   
+
+function toggleActivity() {
+    sidebarActivity.classList.toggle("open-activity");
+
+    if(sidebarActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "Show less <b>-</b>";
+    }else{
+        moreLink.innerHTML = "Show more <b>+</b>";
+    }
+}
+
+
+
